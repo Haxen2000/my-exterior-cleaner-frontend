b4e88d3e9fcc16e8c45b7e11fc7c48e2b767d582
Stephen Synowsky <ssynowsky@gmail.com>
2024-08-29 18:45:54 -0400

    Adding Contact Us page & iframe for form

---
+++ src/app/[lang]/cleaning-solutions-atlanta-ga/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function ContactUsPage() {
+  return (
+    <section className="bg-white text-black contact-us">
+      <link rel="stylesheet" href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css" media="screen" />
+      <div id="bf53a323-eafb-4423-94d0-08cae351a5e9">
+        <iframe
+          className="jobber-work-request mx-auto !visible"
+          src="https://clienthub.getjobber.com/client_hubs/bf53a323-eafb-4423-94d0-08cae351a5e9/public/work_request/embedded_new?source=embedded_inline"
+          sandbox="allow-forms allow-scripts allow-same-origin allow-modals"
+        />
+      </div>
+    </section>
+  );
+}
