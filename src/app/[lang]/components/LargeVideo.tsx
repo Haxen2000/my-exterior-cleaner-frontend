708af99d3bbc13012df5544c5a6f262098f53264
Stephen Synowsky <ssynowsky@gmail.com>
2024-10-07 15:21:39 -0400

    Adding additional video props

--- src/app/[lang]/components/LargeVideo.tsx
+++ src/app/[lang]/components/LargeVideo.tsx
@@ -4,6 +4,9 @@ interface VideoBlockProps {
   video: { data: { attributes: VideoProps } }
   width?: string;
   height?: string;
+  autoplay: boolean;
+  muted: boolean;
+  loop: boolean;
 }
 interface VideoProps {
   id: number;
@@ -20,7 +23,15 @@ export default function LargeVideo({ data }: { data: VideoBlockProps }) {
 
   return (
     <div className="flex justify-center">
-      <video width={data.width || "100%"} height={data.height || "100%"} controls preload="none" className="video-player relative pb-56.25 h-72 lg:h-[450px] overflow-hidden my-8">
+      <video
+        width={data.width || "100%"}
+        height={data.height || "100%"}
+        controls
+        autoPlay={data.autoplay}
+        muted={data.muted}
+        loop={data.loop}
+        className="video-player relative pb-56.25 h-72 lg:h-[450px] overflow-hidden my-8"
+      >
         <source src={videoUrl || ""} type={vidData.mime || ""} />
         Your browser does not support the video tag.
       </video>
